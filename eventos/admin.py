c80dd59ed7c916756977db4f386c1f57c39437cd
Juan <jjmunozg@eafit.edu.co>
2023-09-21 11:24:51 -0500

    Configuracion extra de admin

--- eventos/admin.py
+++ eventos/admin.py
@@ -3,6 +3,19 @@ from .models import Venue
 from .models import User
 from .models import Event
 
-admin.site.register(Venue)
+#admin.site.register(Venue)
 admin.site.register(User)
-admin.site.register(Event)
+#admin.site.register(Event)
+
+@admin.register(Venue)
+class VenueADmin(admin.ModelAdmin):
+    list_display = ('name', 'address')
+    ordering = ('name',)
+    search_fields = ('name', 'address')
+
+@admin.register(Event)
+class EventAdmin(admin.ModelAdmin):
+    fields = (('name','venue'), 'event_date', 'company')
+    list_display = ('name','event_date')
+    list_filter = ('venue', 'event_date' )
+    ordering = ('event_date',)
