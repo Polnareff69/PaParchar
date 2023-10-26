ec00119a6967fb1a81cfe4771245bcdea4f62236
Juan <jjmunozg@eafit.edu.co>
2023-10-26 13:50:27 -0500

    Ahora se puede registrar el usuario

--- usuarios/urls.py
+++ usuarios/urls.py
@@ -7,5 +7,7 @@ from django.conf import settings
 
 urlpatterns = [
     path('login_user', usuariosVista.login_user, name='login_user'),
+    path('logout_user', usuariosVista.logout_user, name='logout_user'),
+    path('register_user', usuariosVista.register_user, name='register_user'),
     
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- usuarios/views.py
+++ usuarios/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth  import authenticate, login, logout
 from django.contrib import messages
+from django.contrib.auth.forms import UserCreationForm
+
 
 def login_user(request):
     if request.method == "POST":
@@ -15,3 +17,23 @@ def login_user(request):
             return redirect('login_user')
     else:
         return render(request, 'autenticacion/login_user.html', {})
+    
+def logout_user(request):
+    logout(request)
+    return redirect('home')
+
+
+def register_user(request):
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password1']
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            return redirect('home')
+    else:
+        form = UserCreationForm()
+
+    return render(request, 'autenticacion/register_user.html', {'form':form})
