cf2cf9e9de10e8b25c5fb9303b5d9328dd351f83
JuanJMunozG <jjmunozg@eafit.edu.co>
2024-03-19 22:40:45 -0500

    Entregable 1

--- usuarios/forms.py
+++ usuarios/forms.py
@@ -10,3 +10,12 @@ class CreateUserForm(UserCreationForm):
     class Meta:
         model = User
         fields = ['username','first_name','email','phone_number','password1','password2']
+
+        widgets = {'username':forms.TextInput(attrs={'class':'form-control','placeholder':'Nombre de usuario'}),
+                  'first_name':forms.TextInput(attrs={'class':'form-control','placeholder':'Nombre'}),
+                  'email':forms.TextInput(attrs={'class':'form-control','placeholder':'Email'}),
+                  'phone_number':forms.TextInput(attrs={'class':'form-control','placeholder':'phone_number'}),
+                  'password1':forms.TextInput(attrs={'class':'form-control', 'placeholder':'Contreaseña'}),
+                  'password2':forms.TextInput(attrs={'class':'form-control', 'placeholder':'Confirma contraseña'}),
+                  }
+        
